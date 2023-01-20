e8ce2e2950a89a47445cc2213aa11ace17b24f63
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-20 14:54:51 +0700

    fix 404.tsx

--- my-website/pages/404.tsx
+++ my-website/pages/404.tsx
@@ -2,7 +2,7 @@ const about = () => {
   return (
     <>
       <div className="flex justify-center py-20">
-        <h1 className="font-bold text-xl">Oops you're out of boundaries</h1>
+        <h1 className="font-bold text-xl">Oops you&apos;re out of boundaries</h1>
       </div>
     </>
   );
