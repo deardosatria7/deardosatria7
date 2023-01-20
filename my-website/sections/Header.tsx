9cb675a59f01b09d75e145139d5214b98b74e9b6
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-20 14:00:07 +0700

    fix dark mode (again)

--- my-website/sections/Header.tsx
+++ my-website/sections/Header.tsx
@@ -7,24 +7,47 @@ export default function Header({ children }: any) {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   const renderThemeChanger = () => {
     if (!mounted) return null;
-    const currentTheme = theme === "system" ? systemTheme : theme;
-    if (currentTheme === "light") {
+
+    const currTheme = theme === "system" ? systemTheme : theme;
+    if (currTheme === "dark") {
       return (
-        <button onClick={() => setTheme("dark")}>
-          <BsSun />
-        </button>
+        <svg // bulb icon
+          className="h-7 w-7 hover:scale-110 cursor-pointer"
+          onClick={() => {
+            setTheme("light");
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
       );
     } else {
       return (
-        <button onClick={() => setTheme("light")}>
-          <MdOutlineNightlight color="white" />
-        </button>
+        <svg // moon icon
+          className="h-7 w-7 hover:scale-110 cursor-pointer"
+          onClick={() => {
+            setTheme("dark");
+          }}
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        </svg>
       );
     }
   };
