1d2f2137b18844c0708c2514652823aa5419c429
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-21 19:00:34 +0700

    update header for project page

--- my-website/pages/projects.tsx
+++ my-website/pages/projects.tsx
@@ -1,6 +1,11 @@
+import Header from "@/sections/Header";
+
 const projects = () => {
   return (
     <>
+      <Header>
+        <h1 className="text-xl font-bold dark:text-white">Projects</h1>
+      </Header>
       <div className="flex justify-center py-20">Under maintenance...</div>
     </>
   );
