0688912005b25822507b633feac3ae0368e61dc0
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-21 19:00:05 +0700

    update header for about page

--- my-website/pages/about.tsx
+++ my-website/pages/about.tsx
@@ -1,6 +1,11 @@
+import Header from "@/sections/Header";
+
 const about = () => {
   return (
     <>
+      <Header>
+        <h1 className="text-xl font-bold dark:text-white">About Me</h1>
+      </Header>
       <div className="flex justify-center py-20">Under maintenance...</div>
     </>
   );
