1de06f50dc4e74ef041a24ebf85334fc961a1d56
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-24 08:15:20 +0700

    fix gap

--- my-website/sections/Header.tsx
+++ my-website/sections/Header.tsx
@@ -52,7 +52,7 @@ export default function Header({ children }: any) {
   return (
     <>
       <nav className="py-10 flex justify-between">
-        <div className="flex gap-7 justify-center">{children}</div>
+        <div className="flex gap-5 justify-center">{children}</div>
         <ul className="flex justify-center gap-2">
           <li>{renderThemeChanger()}</li>
         </ul>
