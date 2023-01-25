7b3a854a27b2fd619cd88530065cd6f26395a2cb
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-25 10:47:16 +0700

    fix for mini screens

--- my-website/sections/Hero.tsx
+++ my-website/sections/Hero.tsx
@@ -29,12 +29,12 @@ const Hero = () => {
                     <span className="font-normal">He/him</span>. Undergraduate student at Sebelas Maret University. There are few projects that I made (mostly just my coursework lol). Love to code, creating errors and bugs everyday.
                   </p>
                 </div>
-                <div className="flex justify-center gap-6 py-1 lg:justify-start lg:px-10">
-                  <a href="/about" className="bg-gray-500 hover:bg-gray-600 text-white text-sm px-5 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">
+                <div className="flex justify-evenly gap-4 lg:justify-start lg:px-10">
+                  <a href="/about" className="bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">
                     About me
                   </a>
                   <Link href="/projects" legacyBehavior>
-                    <a className="bg-gray-500 hover:bg-gray-600 text-white text-sm px-5 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">Projects</a>
+                    <a className="bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">Projects</a>
                   </Link>
                 </div>
                 <div className="flex justify-center gap-8 text-3xl my-3 text-gray-600 lg:justify-start lg:px-10 lg:pt-2">
