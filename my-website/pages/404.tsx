52f0d1648fd2dfe97eff43006fa5b7ed0152f783
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-25 10:37:35 +0700

    Update 404.tsx

--- my-website/pages/404.tsx
+++ my-website/pages/404.tsx
@@ -4,9 +4,9 @@ const about = () => {
   return (
     <>
       <div className="py-20">
-        <h1 className="flex justify-center font-bold text-xl py-2">Oops you&apos;re out of boundaries</h1>
+        <h1 className="flex justify-center font-bold text-xl py-5">Oops you&apos;re out of boundaries</h1>
         <Link href={"/"} className="flex justify-center">
-          <h3 className="bg-gray-400 hover:bg-gray-500 w-36 text-white text-center rounded-md dark:bg-zinc-700 dark:hover:bg-zinc-600">Take me back</h3>
+          <h3 className="bg-gray-400 hover:bg-gray-500 w-36 text-white text-center rounded-full dark:bg-zinc-700 dark:hover:bg-zinc-600">Take me back</h3>
         </Link>
       </div>
     </>
