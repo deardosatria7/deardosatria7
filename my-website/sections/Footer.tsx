78c32adde0602d4c4de0fde7bd79ff4da45eaefa
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-26 10:21:39 +0700

    Update Footer.tsx

--- my-website/sections/Footer.tsx
+++ my-website/sections/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <div className="p-20">
         <footer className="fixed text-center left-0 right-0 bottom-5 justify-center">
-          <div className="text-sm opacity-70 dark:text-gray-300">2023 | Made by Deardo Satria</div>
+          <div className="text-sm opacity-60 dark:text-gray-300">2023 | Made with ❤ by Deardo Satria</div>
         </footer>
       </div>
     </>
