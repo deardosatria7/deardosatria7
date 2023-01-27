2c22e20d135f1752859bb643ac7e5b87abac87bf
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-01-27 10:54:19 +0700

    Update Hero.tsx

--- my-website/sections/Hero.tsx
+++ my-website/sections/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       </Header>
       <section>
         <div className="lg:flex lg:justify-center lg:items-center">
-          <div className="bg-gray-100 rounded-2xl py-4 mb-6 dark:bg-neutral-800">
+          <div className="bg-gray-100 rounded-2xl py-4 dark:bg-neutral-800">
             {/* Profile picture */}
             <div className="lg:flex lg:justify-center lg:pt-10 lg:px-10">
               <div className="relative mx-auto my-5 rounded-full bg-gradient-to-t from-gray-200 to-white w-40 h-40 overflow-hidden lg:w-60 lg:h-60 lg:px-20 lg:my-auto">
@@ -26,7 +26,8 @@ const Hero = () => {
                   <h3 className="font-inter text-3xl text-sky-900 font-medium md:text-4xl dark:text-teal-500">Deardo Satria</h3>
                   <h4 className="font-inter text-md pt-1 pb-5 md:text-lg dark:text-white">Lifelong learner</h4>
                   <p className="mx-auto leading-7 font-sans font-light text-gray-800 text-md md:text-lg max-w-lg dark:text-white">
-                    <span className="font-normal">He/him</span>. Undergraduate student at Sebelas Maret University. There are few projects that I made (mostly just my coursework lol). Love to code, creating errors and bugs everyday.
+                    <span className="font-normal">He/him</span>. Self-taught programming student, focused on front end developers and mobile apps. There are few projects that I made (mostly just my coursework 🤣). Love to code, create
+                    errors and bugs everyday.
                   </p>
                 </div>
                 <div className="flex justify-center mb-4 gap-4 lg:justify-start lg:px-10">
