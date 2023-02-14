0dffaa4b708eca93d0b64c49c6569e244c6c8b07
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-02-14 10:23:29 +0700

    Update about.tsx

--- my-website/pages/about.tsx
+++ my-website/pages/about.tsx
@@ -1,6 +1,6 @@
 import Header from "@/sections/Header";
 import Link from "next/link";
-import { IoChevronBack } from "react-icons/io5";
+import {IoChevronBack} from "react-icons/io5";
 import Footer from "@/sections/Footer";
 
 const about = () => {
@@ -15,14 +15,33 @@ const about = () => {
       <div className="flex flex-col justify-center p-10 bg-gray-100 rounded-xl dark:bg-zinc-800 md:mx-20 lg:mx-40">
         <div className="text-xl font-medium">Hello There!!</div>
         <div className="flex flex-col py-4 gap-y-4 text-gray-600 dark:text-gray-300">
-          <div>Greetings, welcome to my page. My name is Deardo Satria, based in Indonesia. I&apos;m currently in my final year of study majoring in Computer and Information Technology Education.</div>
-          <div>Started coding in 2020, still have a lot to learn. I have interest in UI/UX development, mobile app development, and Python programming language. This website was created using NextJs and tailwindcss.</div>
+          <div>
+            Greetings, welcome to my page. My name is Deardo Satria, based in
+            Indonesia. I&apos;m currently in my final year of study majoring in
+            Computer and Information Technology Education.
+          </div>
+          <div>
+            Started coding in 2020, still have a lot to learn. I have interest
+            in UI/UX development, mobile app development, and Python programming
+            language. This website was created using NextJs and tailwindcss.
+          </div>
           <div>I strive to improve and become better at coding 😁</div>
-          <div className="text-center pt-10 pb-6">Programming languages I&apos;ve used:</div>
+          <div className="text-center pt-10 pb-6">
+            Programming stacks I&apos;ve used:
+          </div>
           <div className="grid grid-cols-3 sm:grid-cols-6 md:grid-cols-7 lg:grid-cols-9 gap-6 place-items-center">
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.python.org/">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="0 0 128 128">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.python.org/"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 128 128"
+                >
                   <g fill-rule="evenodd" clip-rule="evenodd">
                     <path
                       fill="#386f9f"
@@ -41,9 +60,22 @@ const about = () => {
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.java.com/en/">
-                <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="50px" height="50px">
-                  <path fill="#F44336" d="M23.65,24.898c-0.998-1.609-1.722-2.943-2.725-5.455C19.229,15.2,31.24,11.366,26.37,3.999c2.111,5.089-7.577,8.235-8.477,12.473C17.07,20.37,23.645,24.898,23.65,24.898z"></path>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.java.com/en/"
+              >
+                <svg
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 48 48"
+                  width="50px"
+                  height="50px"
+                >
+                  <path
+                    fill="#F44336"
+                    d="M23.65,24.898c-0.998-1.609-1.722-2.943-2.725-5.455C19.229,15.2,31.24,11.366,26.37,3.999c2.111,5.089-7.577,8.235-8.477,12.473C17.07,20.37,23.645,24.898,23.65,24.898z"
+                  ></path>
                   <path
                     fill="#F44336"
                     d="M23.878,17.27c-0.192,2.516,2.229,3.857,2.299,5.695c0.056,1.496-1.447,2.743-1.447,2.743s2.728-0.536,3.579-2.818c0.945-2.534-1.834-4.269-1.548-6.298c0.267-1.938,6.031-5.543,6.031-5.543S24.311,11.611,23.878,17.27z"
@@ -53,57 +85,147 @@ const about = () => {
                       fill="#1565C0"
                       d="M32.084 25.055c1.754-.394 3.233.723 3.233 2.01 0 2.901-4.021 5.643-4.021 5.643s6.225-.742 6.225-5.505C37.521 24.053 34.464 23.266 32.084 25.055zM29.129 27.395c0 0 1.941-1.383 2.458-1.902-4.763 1.011-15.638 1.147-15.638.269 0-.809 3.507-1.638 3.507-1.638s-7.773-.112-7.773 2.181C11.683 28.695 21.858 28.866 29.129 27.395z"
                     ></path>
-                    <path fill="#1565C0" d="M27.935,29.571c-4.509,1.499-12.814,1.02-10.354-0.993c-1.198,0-2.974,0.963-2.974,1.889c0,1.857,8.982,3.291,15.63,0.572L27.935,29.571z"></path>
-                    <path fill="#1565C0" d="M18.686,32.739c-1.636,0-2.695,1.054-2.695,1.822c0,2.391,9.76,2.632,13.627,0.205l-2.458-1.632C24.271,34.404,17.014,34.579,18.686,32.739z"></path>
+                    <path
+                      fill="#1565C0"
+                      d="M27.935,29.571c-4.509,1.499-12.814,1.02-10.354-0.993c-1.198,0-2.974,0.963-2.974,1.889c0,1.857,8.982,3.291,15.63,0.572L27.935,29.571z"
+                    ></path>
+                    <path
+                      fill="#1565C0"
+                      d="M18.686,32.739c-1.636,0-2.695,1.054-2.695,1.822c0,2.391,9.76,2.632,13.627,0.205l-2.458-1.632C24.271,34.404,17.014,34.579,18.686,32.739z"
+                    ></path>
                     <path
                       fill="#1565C0"
                       d="M36.281,36.632c0-0.936-1.055-1.377-1.433-1.588c2.228,5.373-22.317,4.956-22.317,1.784c0-0.721,1.807-1.427,3.477-1.093l-1.42-0.839C11.26,34.374,9,35.837,9,37.017C9,42.52,36.281,42.255,36.281,36.632z"
                     ></path>
-                    <path fill="#1565C0" d="M39,38.604c-4.146,4.095-14.659,5.587-25.231,3.057C24.341,46.164,38.95,43.628,39,38.604z"></path>
+                    <path
+                      fill="#1565C0"
+                      d="M39,38.604c-4.146,4.095-14.659,5.587-25.231,3.057C24.341,46.164,38.95,43.628,39,38.604z"
+                    ></path>
                   </g>
                 </svg>
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://flutter.dev/">
-                <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50px" height="50px" viewBox="0 0 48 48">
-                  <polygon fill="#40c4ff" points="26,4 6,24 12,30 38,4"></polygon>
-                  <polygon fill="#40c4ff" points="38,22 27,33 21,27 26,22"></polygon>
-                  <rect width="8.485" height="8.485" x="16.757" y="28.757" fill="#03a9f4" transform="rotate(-45.001 21 33)"></rect>
-                  <polygon fill="#01579b" points="38,44 26,44 21,39 27,33"></polygon>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://flutter.dev/"
+              >
+                <svg
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 48 48"
+                >
+                  <polygon
+                    fill="#40c4ff"
+                    points="26,4 6,24 12,30 38,4"
+                  ></polygon>
+                  <polygon
+                    fill="#40c4ff"
+                    points="38,22 27,33 21,27 26,22"
+                  ></polygon>
+                  <rect
+                    width="8.485"
+                    height="8.485"
+                    x="16.757"
+                    y="28.757"
+                    fill="#03a9f4"
+                    transform="rotate(-45.001 21 33)"
+                  ></rect>
+                  <polygon
+                    fill="#01579b"
+                    points="38,44 26,44 21,39 27,33"
+                  ></polygon>
                   <polygon fill="#084994" points="21,39 30,36 27,33"></polygon>
                 </svg>
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/HTML">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="0 0 128 128">
-                  <path fill="#E44D26" d="M27.854 116.354l-8.043-90.211h88.378l-8.051 90.197-36.192 10.033z"></path>
-                  <path fill="#F16529" d="M64 118.704l29.244-8.108 6.881-77.076H64z"></path>
-                  <path fill="#EBEBEB" d="M64 66.978H49.359l-1.01-11.331H64V44.583H36.257l.264 2.969 2.72 30.489H64zm0 28.733l-.049.013-12.321-3.328-.788-8.823H39.735l1.55 17.372 22.664 6.292.051-.015z"></path>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 128 128"
+                >
+                  <path
+                    fill="#E44D26"
+                    d="M27.854 116.354l-8.043-90.211h88.378l-8.051 90.197-36.192 10.033z"
+                  ></path>
+                  <path
+                    fill="#F16529"
+                    d="M64 118.704l29.244-8.108 6.881-77.076H64z"
+                  ></path>
+                  <path
+                    fill="#EBEBEB"
+                    d="M64 66.978H49.359l-1.01-11.331H64V44.583H36.257l.264 2.969 2.72 30.489H64zm0 28.733l-.049.013-12.321-3.328-.788-8.823H39.735l1.55 17.372 22.664 6.292.051-.015z"
+                  ></path>
                   <path d="M28.034 1.627h5.622v5.556H38.8V1.627h5.623v16.822H38.8v-5.633h-5.143v5.633h-5.623V1.627zm23.782 5.579h-4.95V1.627h15.525v5.579h-4.952v11.243h-5.623V7.206zm13.039-5.579h5.862l3.607 5.911 3.603-5.911h5.865v16.822h-5.601v-8.338l-3.867 5.981h-.098l-3.87-5.981v8.338h-5.502V1.627zm21.736 0h5.624v11.262h7.907v5.561H86.591V1.627z"></path>
-                  <path fill="#fff" d="M63.962 66.978v11.063h13.624L76.302 92.39l-12.34 3.331v11.51l22.682-6.286.166-1.87 2.6-29.127.27-2.97h-2.982zm0-22.395v11.064h26.725l.221-2.487.505-5.608.265-2.969z"></path>
+                  <path
+                    fill="#fff"
+                    d="M63.962 66.978v11.063h13.624L76.302 92.39l-12.34 3.331v11.51l22.682-6.286.166-1.87 2.6-29.127.27-2.97h-2.982zm0-22.395v11.064h26.725l.221-2.487.505-5.608.265-2.969z"
+                  ></path>
                 </svg>
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://developer.mozilla.org/en-US/docs/Web/CSS">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="-52.5 0 361 361" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid">
-                  <path d="M127.844 360.088L23.662 331.166.445 70.766h255.11l-23.241 260.36-104.47 28.962z" fill="#264DE4"></path>
-                  <path d="M212.417 314.547l19.86-222.49H128V337.95l84.417-23.403z" fill="#2965F1"></path>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://developer.mozilla.org/en-US/docs/Web/CSS"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="-52.5 0 361 361"
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="xMidYMid"
+                >
+                  <path
+                    d="M127.844 360.088L23.662 331.166.445 70.766h255.11l-23.241 260.36-104.47 28.962z"
+                    fill="#264DE4"
+                  ></path>
+                  <path
+                    d="M212.417 314.547l19.86-222.49H128V337.95l84.417-23.403z"
+                    fill="#2965F1"
+                  ></path>
                   <path
                     d="M53.669 188.636l2.862 31.937H128v-31.937H53.669zM47.917 123.995l2.903 31.937H128v-31.937H47.917zM128 271.58l-.14.037-35.568-9.604-2.274-25.471h-32.06l4.474 50.146 65.421 18.16.147-.04V271.58z"
                     fill="#EBEBEB"
                   ></path>
                   <path d="M60.484 0h38.68v16.176H76.66v16.176h22.506v16.175H60.484V0zM106.901 0h38.681v14.066h-22.505v2.813h22.505v32.352h-38.68V34.46h22.505v-2.813H106.9V0zM153.319 0H192v14.066h-22.505v2.813H192v32.352h-38.681V34.46h22.505v-2.813H153.32V0z"></path>
-                  <path d="M202.127 188.636l5.765-64.641H127.89v31.937h45.002l-2.906 32.704H127.89v31.937h39.327l-3.708 41.42-35.62 9.614v33.226l65.473-18.145.48-5.396 7.506-84.08.779-8.576z" fill="#FFF"></path>
+                  <path
+                    d="M202.127 188.636l5.765-64.641H127.89v31.937h45.002l-2.906 32.704H127.89v31.937h39.327l-3.708 41.42-35.62 9.614v33.226l65.473-18.145.48-5.396 7.506-84.08.779-8.576z"
+                    fill="#FFF"
+                  ></path>
                 </svg>
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://www.javascript.com/">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="0 0 128 128">
-                  <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.javascript.com/"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 128 128"
+                >
+                  <path
+                    fill="#F0DB4F"
+                    d="M1.408 1.408h125.184v125.185H1.408z"
+                  ></path>
                   <path
                     fill="#323330"
                     d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zM69.462 58.943H57.753l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"
@@ -112,9 +234,23 @@ const about = () => {
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://getbootstrap.com/">
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50px" height="50px" viewBox="0 0 48 48">
-                  <path fill="#673ab7" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5 V37z"></path>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://getbootstrap.com/"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 48 48"
+                >
+                  <path
+                    fill="#673ab7"
+                    d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5 V37z"
+                  ></path>
                   <path
                     fill="#fff"
                     d="M33.03,25.6c-0.65-0.9-1.59-1.52-2.8-1.85c0,0,1.02-0.37,1.94-1.75c0.55-0.88,0.83-1.94,0.83-3.18 c0-2.15-0.78-3.8-2.34-4.93C29.1,12.76,27.34,12,24.35,12H15v24h10.43c2.83-0.02,4.96-0.63,6.41-1.8c1.44-1.19,2.16-2.95,2.16-5.3 C34,27.6,33.68,26.5,33.03,25.6z M21,16c0,0,4.17,0,4.25,0c1.52,0,2.75,1.23,2.75,2.75c0,1.52-1.23,2.75-2.75,2.75 c-0.08,0-4.25,0-4.25,0V16z M26,32h-5v-6h5c1.66,0,3,1.34,3,3C29,30.66,27.66,32,26,32z"
@@ -123,8 +259,18 @@ const about = () => {
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://tailwindcss.com/">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://tailwindcss.com/"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
                   <title>file_type_tailwind</title>
                   <path
                     d="M9,13.7q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q11.1,10.9,9,13.7ZM2,22.1q1.4-5.6,7-5.6c5.6,0,6.3,4.2,9.1,4.9q2.8.7,4.9-2.1-1.4,5.6-7,5.6c-5.6,0-6.3-4.2-9.1-4.9Q4.1,19.3,2,22.1Z"
@@ -134,8 +280,17 @@ const about = () => {
               </a>
             </div>
             <div>
-              <a target="_blank" rel="noopener noreferrer" href="https://nextjs.org/">
-                <svg fill="currentColor" width="50px" height="50px" viewBox="0 0 128 128">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://nextjs.org/"
+              >
+                <svg
+                  fill="currentColor"
+                  width="50px"
+                  height="50px"
+                  viewBox="0 0 128 128"
+                >
                   <path d="M30.2 45.9h24.1v1.9H32.4v14.4H53v1.9H32.4v15.8h22.2v1.9H30.2V45.9zm26.3 0h2.6l11.4 15.8L82 45.9l15.8-20-26 37.5 13.4 18.4h-2.7L70.4 65 58.2 81.8h-2.6l13.5-18.4-12.6-17.5zm29.7 1.9v-1.9h27.5v1.9H101v34h-2.2v-34H86.2zM0 45.9h2.7l38.2 56.8-15.8-20.9L2.3 48.6l-.1 33.2H0zm113.5 33.4c.5 0 .8-.3.8-.8s-.3-.8-.8-.8-.8.3-.8.8.4.8.8.8zm2.2-2.1c0 1.3 1 2.2 2.4 2.2 1.5 0 2.4-.9 2.4-2.5v-5.5h-1.2v5.5c0 .9-.4 1.3-1.2 1.3-.7 0-1.2-.4-1.2-1.1h-1.2zm6.3-.1c.1 1.4 1.2 2.3 3 2.3s3-.9 3-2.4c0-1.2-.7-1.8-2.2-2.2l-.9-.2c-1-.2-1.4-.6-1.4-1.1 0-.7.6-1.2 1.6-1.2.9 0 1.5.4 1.6 1.2h1.2c-.1-1.3-1.2-2.2-2.8-2.2-1.7 0-2.8.9-2.8 2.3 0 1.1.6 1.8 2 2.1l1 .2c1 .2 1.5.6 1.5 1.2 0 .7-.7 1.2-1.7 1.2s-1.8-.5-1.9-1.2H122z"></path>
                 </svg>
               </a>
