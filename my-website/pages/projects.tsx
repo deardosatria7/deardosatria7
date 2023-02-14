2944f0a3156b038809cc1b1ecbfdcfe2d2a22447
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-02-14 07:13:57 +0700

    update project page

--- my-website/pages/projects.tsx
+++ my-website/pages/projects.tsx
@@ -1,11 +1,12 @@
 import Header from "@/sections/Header";
 import Link from "next/link";
-import { IoChevronBack } from "react-icons/io5";
+import {IoChevronBack} from "react-icons/io5";
 import Card from "@/components/card";
 import Image from "next/legacy/image";
 import Alpha from "../public/alpha.jpg";
 import Semar from "../public/semar.jpg";
 import Mat from "../public/mat.png";
+import pwd from "../public/pwd-gen.jpg";
 import Footer from "@/sections/Footer";
 
 const projects = () => {
@@ -18,47 +19,86 @@ const projects = () => {
         <h1 className="text-xl font-bold dark:text-white">/projects</h1>
       </Header>
       <main>
-        <div className="flex flex-col justify-center md:flex-row md:flex-wrap md:gap-5">
-          <div className="flex-1 basis-1/4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 my-2">
+          <div className="">
             <Card>
-              <div className="dark:opacity-70">
-                <Image src={Alpha} layout="responsive" alt="" />
-              </div>
-              <div className="py-2">
-                <div className="text-lg font-medium pb-2">Bimbel Alpha</div>
-                <div className="text-sm">Website template made with HTML and Bootstrap CSS.</div>
-                <a target="_blank" href="https://deardosatria7.github.io/Bimbel-Alpha" rel="noopener noreferrer">
-                  <span className="text-blue-700 dark:text-blue-400 font-medium text-sm">Click here</span> to see more
-                </a>
-              </div>
+              <a
+                target="_blank"
+                href="https://deardosatria7.github.io/Bimbel-Alpha"
+                rel="noopener noreferrer"
+              >
+                <div className="dark:opacity-70">
+                  <Image src={Alpha} layout="responsive" alt="" />
+                </div>
+                <div className="py-2">
+                  <div className="text-lg font-medium pb-2">Bimbel Alpha</div>
+                  <div className="text-sm">
+                    Website template made with HTML and Bootstrap CSS.
+                  </div>
+                </div>
+              </a>
             </Card>
           </div>
-          <div className="flex-1 basis-1/4">
+          <div className="">
             <Card>
-              <div className="dark:opacity-70">
-                <Image src={Mat} layout="responsive" alt="" />
-              </div>
-              <div className="py-2">
-                <div className="text-lg font-medium pb-2">Math learning media</div>
-                <div className="text-sm">High school mathematics learning media based on flash player.</div>
-                <a target="_blank" href="https://github.com/deardosatria7/matematikaswf" rel="noopener noreferrer">
-                  <span className="text-blue-700 dark:text-blue-400 font-medium text-sm">Click here</span> to see more
-                </a>
-              </div>
+              <a
+                target="_blank"
+                href="https://github.com/deardosatria7/matematikaswf"
+                rel="noopener noreferrer"
+              >
+                <div className="dark:opacity-70">
+                  <Image src={Mat} layout="responsive" alt="" />
+                </div>
+                <div className="py-2">
+                  <div className="text-lg font-medium pb-2">
+                    Math learning media
+                  </div>
+                  <div className="text-sm">
+                    High school mathematics learning media based on flash
+                    player.
+                  </div>
+                </div>
+              </a>
             </Card>
           </div>
-          <div className="flex-1 basis-1/4">
+          <div className="">
             <Card>
-              <div className="dark:opacity-70">
-                <Image src={Semar} layout="responsive" alt="" />
-              </div>
-              <div className="py-2">
-                <div className="text-lg font-medium pb-2">Semar News</div>
-                <div className="text-sm">News portal app for android using Flutter and News API.</div>
-                <a target="_blank" href="https://github.com/Rizakalhasbi/NewsApp" rel="noopener noreferrer">
-                  <span className="text-blue-700 dark:text-blue-400 font-medium text-sm">Click here</span> to see more
-                </a>
-              </div>
+              <a
+                target="_blank"
+                href="https://github.com/Rizakalhasbi/NewsApp"
+                rel="noopener noreferrer"
+              >
+                <div className="dark:opacity-70">
+                  <Image src={Semar} layout="responsive" alt="" />
+                </div>
+                <div className="py-2">
+                  <div className="text-lg font-medium pb-2">Semar News</div>
+                  <div className="text-sm">
+                    News portal app for android using Flutter and News API.
+                  </div>
+                </div>
+              </a>
+            </Card>
+          </div>
+          <div className="">
+            <Card>
+              <a
+                target="_blank"
+                href="https://deardosatria7-password-generator.vercel.app/"
+                rel="noopener noreferrer"
+              >
+                <div className="dark:opacity-70">
+                  <Image src={pwd} layout="responsive" alt="" />
+                </div>
+                <div className="py-2">
+                  <div className="text-lg font-medium pb-2">
+                    Password generator
+                  </div>
+                  <div className="text-sm">
+                    Custom strong password generator.
+                  </div>
+                </div>
+              </a>
             </Card>
           </div>
         </div>
