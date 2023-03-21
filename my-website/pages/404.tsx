60b8e4c1df8c7ce198497074d6cea267a067e24a
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-21 13:44:54 +0700

    Update 404.tsx

--- my-website/pages/404.tsx
+++ my-website/pages/404.tsx
@@ -4,10 +4,9 @@ const about = () => {
   return (
     <>
       <div className="py-20">
-        <h1 className="flex justify-center font-bold text-xl py-5">Oops you&apos;re out of boundaries</h1>
-        <Link href={"/"} className="flex justify-center">
-          <h3 className="bg-gray-400 hover:bg-gray-500 w-36 text-white text-center rounded-full dark:bg-zinc-700 dark:hover:bg-zinc-600">Take me back</h3>
-        </Link>
+        <h1 className="flex justify-center font-bold text-xl py-5">
+          404 | Page not found!
+        </h1>
       </div>
     </>
   );
