e135aa7c63afcdc82912b2306b4d0aebde80d436
Deardo Satria Ristiawan <deardosatria1@gmail.com>
2023-03-29 21:21:07 +0700

    Update Hero.tsx

--- my-website/sections/Hero.tsx
+++ my-website/sections/Hero.tsx
@@ -1,8 +1,8 @@
 import Image from "next/legacy/image";
 import Header from "./Header";
 import Link from "next/link";
-import { AiOutlineInstagram, AiFillGithub } from "react-icons/ai";
-import { BsTelegram } from "react-icons/bs";
+import {AiOutlineInstagram, AiFillGithub} from "react-icons/ai";
+import {BsTelegram} from "react-icons/bs";
 import profilePic from "../public/deardo.jpg";
 
 const Hero = () => {
@@ -17,35 +17,64 @@ const Hero = () => {
             {/* Profile picture */}
             <div className="lg:flex lg:justify-center lg:pt-10 lg:px-10">
               <div className="relative mx-auto my-5 rounded-full bg-gradient-to-t from-gray-200 to-white w-40 h-40 overflow-hidden lg:w-60 lg:h-60 lg:px-20 lg:my-auto">
-                <Image src={profilePic} layout="fill" objectFit="cover" alt={""} />
+                <Image
+                  src={profilePic}
+                  layout="fill"
+                  objectFit="cover"
+                  alt={""}
+                />
               </div>
 
               {/* Description */}
               <div className="lg:flex-col">
                 <div className="text-center px-10 pb-7 lg:text-left">
-                  <h3 className="font-medium text-3xl text-sky-900 md:text-4xl dark:text-teal-400">Deardo Satria</h3>
-                  <h4 className="font-medium text-md pt-1 pb-5 md:text-lg dark:text-white">Lifelong learner</h4>
+                  <h3 className="font-medium text-3xl text-sky-900 md:text-4xl dark:text-teal-400">
+                    Deardo Satria
+                  </h3>
+                  <h4 className="font-medium text-md pt-1 pb-5 md:text-lg dark:text-white">
+                    Lifelong learner
+                  </h4>
                   <p className="mx-auto leading-7 font-light text-gray-800 text-md md:text-lg max-w-lg dark:text-white">
-                    <span className="font-medium">He/him</span>. Self-taught programming student, focused on front end developer and mobile app. There are few projects that I made (mostly just my coursework 🤣). Trying to become one percent
-                    better everyday.
+                    <span className="font-medium">He/him</span>. Self-taught
+                    programming student, focused on front end developer and
+                    mobile app. There are few projects that I made (mostly just
+                    my coursework 🤣). Trying to become a percent better
+                    everyday.
                   </p>
                 </div>
                 <div className="flex justify-center mb-4 gap-4 lg:justify-start lg:px-10">
-                  <a href="/about" className="font-sans bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">
+                  <a
+                    href="/about"
+                    className="font-sans bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500"
+                  >
                     About me
                   </a>
                   <Link href="/projects" legacyBehavior>
-                    <a className="font-sans bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">Projects</a>
+                    <a className="font-sans bg-gray-500 hover:bg-gray-600 text-white text-sm px-3 py-1 rounded-md dark:bg-neutral-700 dark:hover:bg-neutral-500">
+                      Projects
+                    </a>
                   </Link>
                 </div>
                 <div className="flex justify-center gap-8 text-3xl my-3 text-gray-600 lg:justify-start lg:px-10 lg:pt-2">
-                  <a href="https://instagram.com/deardosatria_" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://instagram.com/deardosatria_"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <AiOutlineInstagram />
                   </a>
-                  <a href="https://github.com/deardosatria7" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://github.com/deardosatria7"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <AiFillGithub />
                   </a>
-                  <a href="https://t.me/itsmedeardo" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://t.me/itsmedeardo"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <BsTelegram />
                   </a>
                 </div>
